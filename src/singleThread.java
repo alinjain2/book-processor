f5ddbe075da5c2c26cc9ec6850d51afde38580f7
Alin Jain <alinjain9@gmail.com>
2025-12-03 19:26:43 -0500

    First code commit to singlethread.java
    wrote basic code

--- src/singleThread.java
+++ src/singleThread.java
@@ -1,1 +1,16 @@
+public class singleThread implements Runnable {
 
+    @Override
+    public void run() {
+        System.out.println("Thread is running!");
+    }
+
+    public static void main(String[] args) {
+        singleThread obj = new singleThread();
+        Thread thread1 = new Thread(obj, "Thread1");
+        thread1.start();
+
+
+        System.out.println("Main thread finished.");
+    }
+}
